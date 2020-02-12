c9bc7ff3947beea063c47025336f44a10715a6f8
aTinySeal <atinyseal@gmail.com>
2020-02-12 15:03:13 -0500

    Created rhythm demo

---
+++ New Unity Project/Assets/ParamCube.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParamCube : MonoBehaviour
+{
+    public int _band;
+    public float _startScale, _scaleMultiplier;
+    private void Update()
+    {
+        transform.localScale = new Vector3(transform.localScale.x, (Visualizer._freqBand[_band] * _scaleMultiplier) + _startScale, transform.localScale.z);
+
+        if (Visualizer._freqBand[_band] * _scaleMultiplier >= 10) {
+            Debug.Log("spawn");
+            Vector3 spawnPos = new Vector3(transform.position.x, transform.position.y + 10, transform.position.z - 3);
+            Instantiate(Resources.Load("Sphere"), spawnPos, Quaternion.identity);
+        }
+    
+    }
+}
--- New Unity Project/Assets/Visualizer.cs
+++ New Unity Project/Assets/Visualizer.cs
@@ -7,19 +7,79 @@ public class Visualizer : MonoBehaviour
 {
     AudioSource _audioSource;
     public static float[] _samples = new float[512];
+    public static float[] _freqBand = new float[8];
 
+    float lastTimeSpawned;
     private void Start()
     {
         _audioSource = GetComponent<AudioSource>();
+        lastTimeSpawned = Time.timeSinceLevelLoad;
     }
 
     private void Update()
     {
         GetSpectrumAudioSource();
+        MakeFreqBands();
+
+        for (int i = 0; i < 8; i++) {
+            if (_freqBand[i] > .8f && Time.timeSinceLevelLoad - lastTimeSpawned > .5f)
+            {
+                Debug.Log("Spawned");
+                Vector3 spawnPos = new Vector3(i * 2, 10, 5);
+                Instantiate(Resources.Load("Sphere"), spawnPos, Quaternion.identity);
+
+                lastTimeSpawned = Time.timeSinceLevelLoad;
+            }
+        }
+
     }
 
     void GetSpectrumAudioSource() {
         _audioSource.GetSpectrumData(_samples, 0, FFTWindow.Blackman);
+    }
+
+    void MakeFreqBands() {
+        /*
+         * 22050 / 512 = 43
+         * 
+         * 20 - 60
+         * 250 - 500
+         * 500 - 2000
+         * 2000 - 4000
+         * 4000 - 6000
+         * 
+         * 0 - 2
+         * 1 - 4
+         * 2 - 8
+         * 3 - 16
+         * 4 - 32
+         * 5 - 64
+         * 6 - 128
+         * 7 - 256
+         */
+
+        int count = 0;
+        for (int i = 0; i < 8; i++) {
+            float average = 0;
+            int sampleCount = (int)Mathf.Pow(2, i) *  2;
+
+            if (i == 7) {
+                sampleCount += 2;
+            }
+
+            for (int j = 0; j < sampleCount; j++) {
+                if (count < 512) {
+                    average += _samples[count] * (count++);
+                    count++;
+                }
+            }
+
+            average /= count;
+
+            _freqBand[i] = average * 10;
+
+        }
+
     }
 
 }
