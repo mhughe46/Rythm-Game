be823718f6d247628b3af70778ae456182491183
aTinySeal <atinyseal@gmail.com>
2020-02-18 18:15:47 -0500

    Added Settings script

---
+++ New Unity Project/Assets/Scripts/Settings.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Settings : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public float musicVolume = 1f;
+    public float sfxVolume = 1f;
+    
+
+    private void Update()
+    {
+        mixer.SetFloat("MusicVolume", musicVolume);
+        mixer.SetFloat("SFXVolume", sfxVolume);
+    }
+
+
+}
