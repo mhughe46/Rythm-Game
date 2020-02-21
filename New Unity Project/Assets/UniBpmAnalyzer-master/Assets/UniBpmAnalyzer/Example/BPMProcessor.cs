3049e4e6c753fe07e518a4a221d4203424d9f41c
aTinySeal <atinyseal@gmail.com>
2020-02-20 19:35:02 -0500

    Added BPM detection

---
+++ New Unity Project/Assets/UniBpmAnalyzer-master/Assets/UniBpmAnalyzer/Example/BPMProcessor.cs
@@ -0,0 +1,26 @@
+﻿/*
+UniBpmAnalyzer
+Copyright (c) 2016 WestHillApps (Hironari Nishioka)
+This software is released under the MIT License.
+http://opensource.org/licenses/mit-license.php
+*/
+
+using UnityEngine;
+
+public class BPMProcessor : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip targetClip;
+    public int BPM;
+
+    private void Start()
+    {
+        int bpm = BMPAnalysis.AnalyzeBpm(targetClip);
+        if (bpm < 0)
+        {
+            Debug.LogError("AudioClip is null.");
+            return;
+        }
+        BPM = bpm;
+    }
+}
