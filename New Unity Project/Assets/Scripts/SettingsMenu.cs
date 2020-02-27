a71538fad41a86c0ddee0e52f83120f7989e1ab0
spyjack <terranorion@gmail.com>
2020-02-27 18:52:42 -0500

    Settings v1

---
+++ New Unity Project/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    [SerializeField]
+    AudioMixer audioMixer;
+
+    Resolution[] resolutions;
+
+    [SerializeField]
+    Dropdown resolutionDropdown = null;
+
+    private void Start()
+    {
+        GetResolutions();
+    }
+
+    private void GetResolutions()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetMusicLevel(float sliderValue)
+    {
+        audioMixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 20);
+    }
+
+    public void SetSoundLevel(float sliderValue)
+    {
+        audioMixer.SetFloat("SFXVolume", Mathf.Log10(sliderValue) * 20);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+}
